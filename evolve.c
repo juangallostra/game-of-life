5eb0164bbf59d48b4ed62f14a51ad665a280cd71
Victor Centellas <Victor@client75-10-193-124.xsfupc.upc.edu>
2017-10-19 20:33:08 +0200

    Structure definition

--- evolve.c
+++ evolve.c
@@ -3,15 +3,30 @@
 
 // Variable declaration
 
+// definir la nueva matriz que voy a utilizar
+
+// matriz no, [2D array]
 
 // Function declaration
 
+// Function definition
 
-// Main function
-int main(void)
+*matrix evolve(*matrix)
 {
 
+	// Si queremos hacer una matrix 100x100, la haremos 102x102 para
+	// poder saber la informacion de los bordes y evitar errores
+
+	// 1. Recuperar la matriz
+
+	// 2. Doble for para acceder a cada celda
+
+	// 3. Conjunto de IF para saber el el siguiente estado
+
+
+	// Crear nueva matriz 
+
+	// Crear pointer de esta nueva matriz y asociarlo
+
+	// Return el pointer
 }
-
-
-// Function definition
