c87a2f944396bba821854f5e5a42a74b6118fa98
juangallostra <juangallostra@gmail.com>
2017-10-30 20:45:34 +0100

    Code cleaning and improved clearness

--- main.c
+++ main.c
@@ -15,15 +15,6 @@ int main(void)
 	int pattern;
 	tuple *next_state;
 
-	// unsigned short *pnew_state = (unsigned short *)malloc(6*sizeof(unsigned short));
-
-	// *(pnew_state = (1<<8)| 2;
-	// *(pnew_state + 1) = (2<<8)| 3;
-	// *(pnew_state + 2) = (3<<8)| 1;
-	// *(pnew_state + 3) = (3<<8)| 2;
-	// *(pnew_state + 4) = (3<<8)| 3;
-	// *(pnew_state + 5) = TERMINATOR;
-
 	printf("Insert pattern from 1 to 3:\n");
 	scanf("%i",&pattern);
 	switch (pattern) 
@@ -31,42 +22,42 @@ int main(void)
 		case 1:/*The R-pentomino */
 			length = 6;
 			pnew_state = malloc(length*sizeof(unsigned short));
-			*(pnew_state) =  ((20<<8)|85+1);
-			*(pnew_state + 1) = ((20+1<<8)|85);
-			*(pnew_state + 2) = ((20+1<<8)|85+1);
-			*(pnew_state + 3) = ((20+2<<8)|85+1);
-			*(pnew_state + 4) = ((20<<8)|85+2);
+			*(pnew_state) =  ((110<<8))|(175+1);
+			*(pnew_state + 1) = ((110+1)<<8)|(175);
+			*(pnew_state + 2) = ((110+1)<<8)|(175+1);
+			*(pnew_state + 3) = ((110+2)<<8)|(175+1);
+			*(pnew_state + 4) = ((110<<8))|(175+2);
 			break;
 		case 2:	/*Diehard*/
 			length = 8;
 			pnew_state = malloc(length*sizeof(unsigned short));
-			*(pnew_state) = ((20+1<<8)|85-2);
-			*(pnew_state + 1) =  ((20+1<<8)|85-1);
-			*(pnew_state + 2) = ((20+2<<8)|85-1);
-			*(pnew_state + 3) = ((20<<8)|85+4);
-			*(pnew_state + 4) = ((20+2<<8)|85+3);
-			*(pnew_state + 5) = ((20+2<<8)|85+4);
-			*(pnew_state + 6) = ((20+2<<8)|85+5);
+			*(pnew_state) = ((110+1)<<8)|(175-2);
+			*(pnew_state + 1) =  ((110+1)<<8)|(175-1);
+			*(pnew_state + 2) = ((110+2)<<8)|(175-1);
+			*(pnew_state + 3) = (110<<8)|(175+4);
+			*(pnew_state + 4) = ((110+2)<<8)|(175+3);
+			*(pnew_state + 5) = ((110+2)<<8)|(175+4);
+			*(pnew_state + 6) = ((110+2)<<8)|(175+5);
 			break;
 		case 3: /*Acorn*/
 			length = 8;
 			pnew_state = malloc(length*sizeof(unsigned short));
-			*(pnew_state) = ((20+1<<8)|85+1);
-			*(pnew_state + 1) = ((20<<8)|85-1);
-			*(pnew_state + 2) = ((20+2<<8)|85-1);
-			*(pnew_state + 3) = ((20+2<<8)|85-2);
-			*(pnew_state + 4) = ((20+2<<8)|85+2);
-			*(pnew_state + 5) = ((20+2<<8)|85+3);
-			*(pnew_state + 6) = ((20+2<<8)|85+4);
+			*(pnew_state) = ((110+1)<<8)|(175+1);
+			*(pnew_state + 1) = ((110<<8))|(175-1);
+			*(pnew_state + 2) = ((110+2)<<8)|(175-1);
+			*(pnew_state + 3) = ((110+2)<<8)|(175-2);
+			*(pnew_state + 4) = ((110+2)<<8)|(175+2);
+			*(pnew_state + 5) = ((110+2)<<8)|(175+3);
+			*(pnew_state + 6) = ((110+2)<<8)|(175+4);
 			break;
 		case 4: /* Glider */
 			length = 6;
 			pnew_state = malloc(length*sizeof(unsigned short));
-			*(pnew_state) = (1<<8)| 2;
-			*(pnew_state + 1) = (2<<8)| 3;
-			*(pnew_state + 2) = (3<<8)| 1;
-			*(pnew_state + 3) = (3<<8)| 2;
-			*(pnew_state + 4) = (3<<8)| 3;
+			*(pnew_state) = ((110+1)<<8) | (175+2);
+			*(pnew_state + 1) = ((110+2)<<8) | (175+3);
+			*(pnew_state + 2) = ((110+3)<<8) | (175+1);
+			*(pnew_state + 3) = ((110+3)<<8) | (175+2);
+			*(pnew_state + 4) = ((110+3)<<8) | (175+3);
 			break;
 		default:
 			printf("Invalid pattern\n");
@@ -74,18 +65,18 @@ int main(void)
 	}
 
 	*(pnew_state + length - 1) = TERMINATOR;
-
 	while(1)
 	{
 		// Call the function to draw the actual state
 		draw(pnew_state, TERMINATOR);
 		// Call the function to compute the next state 
-		next_state = evolve(pnew_state, ROW, COLUMN, TERMINATOR);
+		next_state = evolve(pnew_state, length, ROW, COLUMN, TERMINATOR);
 		free(pnew_state);
 		// Copy all the infor*(pnew_state(ion stored from the pointer stateUpdate 
 		// till the size to the pointer pnew_state
 		pnew_state = malloc((*next_state).length*sizeof(*pnew_state));
 		memcpy(pnew_state, (*next_state).state, (*next_state).length*sizeof(*pnew_state));
+		length = (*next_state).length;
 		// Free memory
 		free((*next_state).state);
 		free(next_state);
